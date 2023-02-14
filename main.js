d1cf3f8c2a0c895409f9dfab455aa35ffa017a80
James Brooks <virverum22@gmail.com>
2023-02-14 14:24:41 -0500

    Loop exercise to be completed after conditionals chapter

---
+++ main.js
@@ -0,0 +1,13 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+for(var i = 0; i < cohortOneStudents.length; i++){
+    console.log(cohortOneStudents[i]);
+}
+
+var myInterests = ["Reading", "Video Games", "Tabletop RPGS", "Craft Beer", "Writing"]
+
+
+for(var i =0; i < myInterests.length; i++){
+    console.log("One of my interests is:" + myInterests[i])
+}
+
