54a05f985e9de8e9825211b63115d21dd96784e7
James Brooks <virverum22@gmail.com>
2023-02-15 10:33:55 -0500

    Completed Loop exercise (with help from Tommy)

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
 
 for(var i = 0; i < cohortOneStudents.length; i++){
-    console.log(cohortOneStudents[i]);
+    // console.log(cohortOneStudents[i]);
 }
 
-var myInterests = ["Reading", "Video Games", "Tabletop RPGS", "Craft Beer", "Writing"]
-
-
+var myInterests = ["sleeping", "eating", "Reading", "Video Games", "Tabletop RPGS", "Craft Beer", "Writing"]
+                             
 for(var i =0; i < myInterests.length; i++){
-    console.log("One of my interests is:" + myInterests[i])
-}
 
+    if("Reading" === myInterests[i]){
+    console.log("My absolute favorite interests is:" + myInterests[i])
+    }
+    else {
+     console.log("One of my interests is:" + myInterests[i])
+    }
+
+}                              
